eef2beb806521842c2bec946647ffc8ab0142056
Dineshkumaar07 <dineshkumaar2003@gmail.com>
2023-07-17 18:33:02 +0530

    Bcrypt to bycryptjs

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/UserModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 //Create User POST Request:
